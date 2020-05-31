bd9308f95618e7cc5f7164d80f52ac05f45ed045
mdtgupta939 <mdtgupta939@gmail.com>
2020-05-31 18:58:32 +0530

    new changes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.7@sha256:8c03bb07a531c53ad7d0f6e7041b64d81f99c6e493cb39abba56d956b40eacbc
 
-MAINTAINER Leonardo Gatica <lgatica@protonmail.com>
+MAINTAINER Mudit Porwal <mdtgupta939@gmail.com>
 
 RUN apk add --no-cache mongodb-tools py2-pip && \
   pip install pymongo awscli && \
--- backup.sh
+++ backup.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env sh
 
-OPTIONS=`python /usr/local/bin/mongouri`
+#OPTIONS=`python /usr/local/bin/mongouri`
 BACKUP_NAME="$(date -u +%Y-%m-%d_%H-%M-%S)_UTC.gz"
 
 # Run backup
-mongodump ${OPTIONS} -o /backup/dump
+mongodump --uri=$MONGO_URI --gzip --excludeCollection GLLogs --excludeCollection Resource --excludeCollection UserLogs --excludeCollection UserLogsTOLL --excludeCollection userIdToken -o /backup/dump
 # Compress backup
 cd /backup/ && tar -cvzf "${BACKUP_NAME}" dump
 # Upload backup
 aws s3 cp "/backup/${BACKUP_NAME}" "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}"
 # Delete temp files
 rm -rf /backup/dump
+
+#Slack nontification
+echo --Sending slack Notification for $DBNAME
+curl -X POST -H 'Content-type: application/json' \
+--data '{"text":"'$DBNAME' backup done and uploaded to S3"}' \
+$SLACK_URL
 
 # Delete backup files
 if [ -n "${MAX_BACKUPS}" ]; then
