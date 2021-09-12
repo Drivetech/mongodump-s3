935c79c6d903d308b172a23dbf78404e06ca2990
vik-y <vikasyadavgood@gmail.com>
2021-09-12 19:50:42 +0530

    Adds support for adding endpoint-url
    Having endpoint url can allow reusing the same repo to push backed up
    files to any s3 compatiable storage like minio, digitalocean, etc.

--- README.md
+++ README.md
@@ -103,6 +103,7 @@ You need to add a user with the following policies. Be sure to change `your_buck
 - `S3_PATH` - Default value is `mongodb`. Example `s3://your_bucket/mongodb`
 - `MONGO_COMPLETE` - Default not set. If set doing backup full mongodb
 - `MAX_BACKUPS` - Default not set. If set doing it keeps the last n backups in /backup
+- `AWS_S3_ENDPOINT` - Default not set. You can use this for backing up to other s3 compatible storages
 
 ## License
 
--- backup.sh
+++ backup.sh
@@ -8,7 +8,13 @@ mongodump ${OPTIONS} -o /backup/dump
 # Compress backup
 cd /backup/ && tar -cvzf "${BACKUP_NAME}" dump
 # Upload backup
-aws s3 cp "/backup/${BACKUP_NAME}" "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}"
+
+# If the AWS_S3_ENDPOINT variable isn't empty, then populate the --endpoint-url parameter to use a custom S3 compatable endpoint
+if [ ! -z "$AWS_S3_ENDPOINT" ]; then
+    ENDPOINT="--endpoint-url=$AWS_S3_ENDPOINT"
+fi
+
+aws $ENDPOINT s3 cp "/backup/${BACKUP_NAME}" "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}"
 # Delete temp files
 rm -rf /backup/dump
 
